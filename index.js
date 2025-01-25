9ea3efa82f8129682aa756035abc62ca5c240397
hajilok <120608486+hajilok@users.noreply.github.com>
2025-01-25 13:42:24 +0700

    add method apikey

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const main = async () => {
         .split('\n')
         .filter(Boolean);
     
-        
+    const api = (await fs.readFile('api.txt', 'utf-8'))
+        .replace(/\r/g, "")
+        .split('\n')
+        .filter(Boolean)
 
     while (true) {
         for (let i = 0; i < url.length; i++) {
-            const postUrl = await axios.post(`${url[i]}/v1/chat/completions`, { "messages":[{"role":"system", "content": "You are a helpful assistant."}, {"role":"user", "content": "Hello siapakah kamu "}] } );
+            const postUrl = await axios.post(`${url[i]}/v1/chat/completions`, { "messages":[{"role":"system", "content": "You are a helpful assistant."}, {"role":"user", "content": "Hello siapakah kamu "}] }, {headers: { Authorization: api }});
             const response = postUrl.data;
             console.log(response.choices[0].message);
         }
