031d93fc72475a1fcf22c16fa14ffcb790b045cd
hajilok <120608486+hajilok@users.noreply.github.com>
2025-03-07 09:32:12 +0700

    Update: add new method header to get chat message on AI

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-import axios from 'axios';
-import fs from 'fs/promises';
+import axios from "axios";
+import fs from "fs/promises";
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const main = async () => { 
-    const url = (await fs.readFile('url.txt', 'utf-8'))
-        .replace(/\r/g, "")
-        .split('\n')
-        .filter(Boolean);
-    
-    const api = (await fs.readFile('api.txt', 'utf-8'))
-        .replace(/\r/g, "")
-        .split('\n')
-        .filter(Boolean)
+const main = async () => {
+  const url = (await fs.readFile("url.txt", "utf-8"))
+    .replace(/\r/g, "")
+    .split("\n")
+    .filter(Boolean);
 
-    while (true) {
-        for (let i = 0; i < url.length; i++) {
-           try {
-            const postUrl = await axios.post(`${url[i]}/v1/chat/completions`, { "messages":[{"role":"system", "content": "You are a helpful assistant."}, {"role":"user", "content": "Hello siapakah kamu "}] }, {headers: { Authorization: api }});
-            const response = postUrl.data;
-            console.log(response.choices[0].message);
-           } catch (error) {
-             if (error.response.status === 500) {
-                console.log('Rate limit exceeded, please wait 1 minute');
-             } else {
-                console.log('Error, please check your url or api');
-             }
-           }
+  const api = (await fs.readFile("api.txt", "utf-8"))
+    .replace(/\r/g, "")
+    .split("\n")
+    .filter(Boolean);
+
+  while (true) {
+    for (let i = 0; i < url.length; i++) {
+      try {
+        const postUrl = await axios.post(
+          `${url[i]}/v1/chat/completions`,
+          {
+            messages: [
+              { role: "system", content: "You are a helpful assistant." },
+              { role: "user", content: "Hello siapakah kamu " },
+            ],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${api}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const response = postUrl.data;
+        console.log(response.choices[0].message);
+      } catch (error) {
+        if (error.response.status === 500) {
+          console.log("Rate limit exceeded, please wait 1 minute");
+        } else {
+          console.log(error);
+          console.log("Error, please check your url or api");
         }
-        await delay(60000);
+      }
     }
-   
-} 
+    await delay(60000);
+  }
+};
 
 main();
